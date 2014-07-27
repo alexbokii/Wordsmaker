18165e96f9004956625af3109c602d2356bfec98
Alex <vanilinka33@gmail.com>
2014-07-27 17:46:59 +0100

    Adding styles for browsers, adding new functions

--- scripts/jquery.js
+++ scripts/jquery.js
@@ -4,23 +4,53 @@ $(document).ready(function() {
   var generalLibrary = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","w","x","y","z"];
   var currentLibrary = [];
   var myCurrentWord = "";
+  var myListOfWords = [];
   var myPoints = 0;
   var currentPoint = 0;
-  var a = setInterval(makeTimer, 1000);
+  var timerID;
+  var minutes;
+  var seconds;
 
 
-  function createNewGame() {
+  function createCurrentLibrary() {
     for(var i = 0; i<16; i++) {
       var currentLetter = generalLibrary[Math.floor(Math.random()*generalLibrary.length)];
       currentLibrary.push(currentLetter);
     }
+    console.log(currentLibrary);
+    checkCurrentLibrary();
+  }
+
+  function checkCurrentLibrary() {
+    var numberOfVowels = 0;
+    for (var i = 0; i<currentLibrary.length; i++) { 
+      if(currentLibrary[i] === "a"||currentLibrary[i] === "o"||currentLibrary[i] === "e"||currentLibrary[i] === "i"||currentLibrary[i] === "u") {
+        numberOfVowels++;
+      }
+    }
+    console.log(numberOfVowels);
+    if(numberOfVowels < 2 || numberOfVowels > 5) {
+      console.log("Restricted number of vowels!");
+      currentLibrary = [];
+      createCurrentLibrary();
+    }  
+  } //end of checkCurrentLibrary (check how many vowels we have)
+
+  function createNewGame() {
+    timerID = setInterval(makeTimer, 1000); //set interval
+    createCurrentLibrary();
+    
   } //end of createNewGame
 
   function resetOldGame() {
+    minutes = 3;
+    seconds = 00;
     currentLibrary = [];
+    myListOfWords = [];
     myCurrentWord = "";
     myPoints = 0;
     currentPoint = 0;
+    $("li").removeClass("is-active");
     $(".points").text("");
     $("#word-field").text("");
     $(".words-list").empty();
@@ -28,15 +58,16 @@ $(document).ready(function() {
   } //end of resetOldGame
 
   function createWord() {
+     $("p").removeClass('is-existed');  //in case previous word was repeated
     document.getElementById('audiotag1').play();  //play sound
     var currentLetter = $(this).text();
-    if($(this).hasClass("active-letter") === true) {
+    if($(this).hasClass("is-active") === true) {
       document.getElementById('same-letter').play();
-      $(this).removeClass("active-letter");
+      $(this).removeClass("is-active");
       myCurrentWord = myCurrentWord.replace(currentLetter, '');
     }
     else {
-      $(this).addClass("active-letter");
+      $(this).addClass("is-active");
       myCurrentWord = myCurrentWord + currentLetter
     }
     $('#word-field').text(myCurrentWord);
@@ -49,6 +80,18 @@ $(document).ready(function() {
       function(data) {
         console.log(data);
         if (data.tuc) {
+          //if we already used this word
+          for (var i = 0; i < myListOfWords.length; i++) {
+            if (myCurrentWord === myListOfWords[i]) {
+              $("p:contains('" + myCurrentWord + "')").addClass('is-existed');
+              clearField();
+              return;
+            }
+          } //end of for
+          //if word is new
+         
+          myListOfWords.push(myCurrentWord);
+          console.log(myListOfWords);
           $('.mark').addClass('win-mark');
           document.getElementById('correct').play();
           setInterval(function(){
@@ -75,15 +118,12 @@ $(document).ready(function() {
     if(myCurrentWord.length === 3) {
       currentPoint = 2;
     }
-
     else if (myCurrentWord.length === 4) {
       currentPoint = 3;
     }
-
     else if (myCurrentWord.length === 5) {
       currentPoint = 4;
     }
-
     else {
       currentPoint = 5;
     }
@@ -93,18 +133,16 @@ $(document).ready(function() {
 
   function clearField() {
     myCurrentWord = "";
-    $("li").removeClass("active-letter");
+    $("li").removeClass("is-active");
     $('#word-field').text("");
-  }
+  } // end of clearField
 
   //2. Click "start" and make random library
   $(".start-button").on("click", function() {
-     $(".finish").hide();
-    $(".landing-container").hide();
-   
+    $(".finish").hide();
+    $(".landing-container").hide(); 
     resetOldGame();
     createNewGame();
-    makeTimer();  // not sure about it
     // fill html with this letters
     currentLibrary.forEach(function(letter, index) {
       $(".letter_" + (index + 1)).html(letter);
@@ -121,25 +159,28 @@ $(document).ready(function() {
   $("#clear").on('click', clearField);
 
   //6. Set timer
-  var minutes = 1;
-  var seconds = 00;
-
   function makeTimer() {
-     seconds--;
-    if(seconds < 10 && seconds >= 0) {
+    seconds--;
+    if (minutes === 0 && seconds === 0) {
+      myStopFunction();
+      showEnd();
+      minutes = 3;
+      seconds = "00";
+     }
+     else if(seconds < 10 && seconds >= 0) {
       seconds = "0" + seconds;
      }
      else if(seconds === -1 && minutes != 0) {
       minutes--;
       seconds = 59;
      }
-     else if (minutes === 0 && seconds === -1) {
-      clearTimeout(a);
-      showEnd();
-     }
      $("#timer").text(minutes + " : " + seconds);
      seconds = parseInt(seconds);
   }
+
+  function myStopFunction() {
+    clearInterval(timerID);
+}
 
   function showEnd() {
     $(".finish").show();
